292d5ade3dd62acd590408a4db144fbb38aec1cb
Leah Atkins <leah.atkins84@gmail.com>
2022-02-10 16:58:50 -0600

    Added renderData function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -34,6 +34,7 @@ function getKoalas(){
   }).then(function(response){
     console.log('Got a response', response);
     //Call a function here
+    renderData();
   }).catch(function(err) {
     console.log('Unable to get Koalas', err);
     
@@ -46,4 +47,20 @@ function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
+}
+
+function renderData() {
+  console.log('in renderData');
+  for (let koala of koalas){
+    $('#viewKoalas').append(`
+    <tr data-id=${koala[i].id}>
+        <td>${koala[i].name}</td>
+        <td>${koala[i].age}</td>
+        <td>${koala[i].gender}</td>
+        <td>${koala[i].readyForTransferIn}</td>
+        <td>${koala[i].notes}</td>
+    </tr>
+    
+    `)
+  }
 }
